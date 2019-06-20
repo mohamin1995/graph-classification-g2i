4cb92ea8b965c076025699f081d4442befd08e98
m.amin <m.amin.ce.sbu@gmail.com>
2019-06-20 14:01:00 +0430

    packages created and some utils implemented

--- graph_classification_g2i_demo/Demo.py
+++ graph_classification_g2i_demo/Demo.py
@@ -1,106 +1,109 @@
 import sys, getopt
-from graph_classification_g2i_util.FileUtil import FileUtil
+from graph_classification_g2i_util.FileUtil import *
+from graph_classification_g2i_util.GraphUtil import *
 from graph_classification_g2i_config.ConfigHandler import Config
 from graph_classification_g2i_classificaion.ClassificationUtil import ClassificationUtil
-from sklearn.model_selection import train_test_split, cross_val_score, LeaveOneOut
+from sklearn.model_selection import  LeaveOneOut
 
 import numpy as np
 
 
-#argv = '-h 45 -m  87 -d iu'.split()
+argv = '-c D:/conf.ini'.split()
 
-#try:
-    #       opts, args = getopt.getopt(argv, 'h:m: d:')
+def main(args):
+    try:
+        pts, args = getopt.getopt(argv, 'c:')
+        config = Config(pts[0][1])
 
+        fmri_asd_path = config.get('fmri_asd_path')
+        fmri_td_path = config.get('fmri_td_path')
+        brain_net_file_format = config.get('brain_net_file_format')
 
-fmri_asd_path = Config().get('fmri_asd_path')
-fmri_td_path = Config().get('fmri_td_path')
-brain_net_file_format = Config().get('brain_net_file_format')
+        g_util = GraphUtil()
 
-
-
-
-X = []
-Y = []
-
-for k in range(1, 41):
-    print('[' + str(k) + '] ASD subject processed successfully...')
-    n = FileUtil(fmri_asd_path + str(k) + brain_net_file_format).get_brain_matrix_from_file()
-    G = get_filtered_matrix_fmri(n)
-
-    sortedDic = sorted(G.degree, key=lambda x: x[1], reverse=True)
-
-    m = len(sortedDic)
-    newAdjMatrix = np.zeros((m, m), np.uint8)
-
-    for i in range(0, m):
-        for j in range(0, m):
-            if (G.has_edge(sortedDic[i][0], sortedDic[j][0])):
-                newAdjMatrix[i][j] = 255
-            else:
-                newAdjMatrix[i][j] = 0
-
-    #cv2.imshow('im', newAdjMatrix)
-    #cv2.waitKey()
-
-    X.append(newAdjMatrix.flatten())
-    Y.append(1)
-
-for k in range(1, 38):
-    print('[' + str(k) + '] TD subject processed successfully...')
-    n = FileUtil(fmri_td_path + str(k) + brain_net_file_format).get_brain_matrix_from_file()
-    G = get_filtered_matrix_fmri(n)
-
-    sortedDic = sorted(G.degree, key=lambda x: x[1], reverse=True)
-
-    m = len(sortedDic)
-    newAdjMatrix = np.zeros((m, m), np.uint8)
-
-    for i in range(0, m):
-        for j in range(0, m):
-            if (G.has_edge(sortedDic[i][0], sortedDic[j][0])):
-                newAdjMatrix[i][j] = 255
-            else:
-                newAdjMatrix[i][j] = 0
-
-    #cv2.imshow('im', newAdjMatrix)
-    #cv2.waitKey()
-
-    X.append(newAdjMatrix.flatten())
-    Y.append(0)
+        X = []
+        Y = []
 
 
-X_array = np.array(X)
-y_array = np.array(Y)
+        for k in range(1, 41):
+            n = FileUtil(fmri_asd_path + str(k) + brain_net_file_format).get_brain_matrix_from_file()
+            G = g_util.get_filtered_matrix_fmri(n,config)
+
+            sortedDic = sorted(G.degree, key=lambda x: x[1], reverse=True)
+
+            m = len(sortedDic)
+            newAdjMatrix = np.zeros((m, m), np.uint8)
+
+            for i in range(0, m):
+                for j in range(0, m):
+                    if (G.has_edge(sortedDic[i][0], sortedDic[j][0])):
+                        newAdjMatrix[i][j] = 255
+                    else:
+                        newAdjMatrix[i][j] = 0
+
+            #cv2.imshow('im', newAdjMatrix)
+            #cv2.waitKey()
+
+            X.append(newAdjMatrix.flatten())
+            Y.append(1)
+            print('[' + str(k) + '] ASD subject processed successfully...')
+
+
+        for k in range(1, 38):
+            n = FileUtil(fmri_td_path + str(k) + brain_net_file_format).get_brain_matrix_from_file()
+            G = g_util.get_filtered_matrix_fmri(n,config)
+
+            sortedDic = sorted(G.degree, key=lambda x: x[1], reverse=True)
+
+            m = len(sortedDic)
+            newAdjMatrix = np.zeros((m, m), np.uint8)
+
+            for i in range(0, m):
+                for j in range(0, m):
+                    if (G.has_edge(sortedDic[i][0], sortedDic[j][0])):
+                        newAdjMatrix[i][j] = 255
+                    else:
+                        newAdjMatrix[i][j] = 0
+
+            #cv2.imshow('im', newAdjMatrix)
+            #cv2.waitKey()
+
+            X.append(newAdjMatrix.flatten())
+            Y.append(0)
+            print('[' + str(k) + '] TD subject processed successfully...')
 
 
 
-loo = LeaveOneOut()
-ytests = []
-ypreds = []
-
-for train_idx, test_idx in loo.split(X):
-    X_train, X_test = X_array[train_idx], X_array[test_idx]  # requires arrays
-    y_train, y_test = y_array[train_idx], y_array[test_idx]
-    model =  ClassificationUtil.get_dt_model()
-    model.fit(X=X_train, y=y_train)
-    y_pred = model.predict(X_test)
-    ytests += list(y_test)
-    ypreds += list(y_pred)
-
-acc = ClassificationUtil.get_accuracy(ytests, ypreds)
-prec = ClassificationUtil.get_precision(ytests, ypreds)
-rec = ClassificationUtil.get_recall(ytests, ypreds)
+        X_array = np.array(X)
+        y_array = np.array(Y)
 
 
-print('accuracy = '+str(acc))
-print('precision = '+str(prec))
-print('recall = '+str(rec))
+
+        loo = LeaveOneOut()
+        ytests = []
+        ypreds = []
+
+        for train_idx, test_idx in loo.split(X):
+            X_train, X_test = X_array[train_idx], X_array[test_idx]  # requires arrays
+            y_train, y_test = y_array[train_idx], y_array[test_idx]
+            model =  ClassificationUtil.get_dt_model(X_train,y_train)
+            y_pred = ClassificationUtil.classify(model,X_test)
+            ytests += list(y_test)
+            ypreds += list(y_pred)
+
+        acc = ClassificationUtil.get_accuracy(ytests, ypreds)
+        prec = ClassificationUtil.get_precision(ytests, ypreds)
+        rec = ClassificationUtil.get_recall(ytests, ypreds)
 
 
-#except getopt.GetoptError:
-#        print('Something went wrong!')
-#        sys.exit(2)
+        print('accuracy = '+str(acc))
+        print('precision = '+str(prec))
+        print('recall = '+str(rec))
 
-#if __name__ == "__main__":
-#   main(sys.argv[1:])
+
+    except getopt.GetoptError:
+           print('Something went wrong!')
+           sys.exit(2)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ graph_classification_g2i_util/GraphUtil.py
@@ -0,0 +1,37 @@
+from graph_classification_g2i_config.ConfigHandler import Config
+import networkx as nx
+import logging
+
+class GraphUtil:
+
+
+
+
+    def get_graph_from_matrix(self,n):
+
+        try:
+            G = nx.from_numpy_matrix(n)
+            return G
+        except Exception as e:
+            print('[Log]: Cant create graph from matrix')
+            logging.exception(e)
+            return None
+
+
+    def get_filtered_matrix_fmri(self,n,config):
+
+        # set sparsity to 24
+        # binarization
+
+        num_of_max_edges = (len(n) * (len(n) - 1)) / 2
+        threshold = 0
+
+        for i in range(0, 100):
+            remaining_num = (n > threshold).sum()
+            sparsity = (remaining_num / num_of_max_edges) * 100
+            if abs(sparsity - (int)(config.get('fmri_sparsity_threshold'))) < 0.5:
+                break
+            threshold = threshold + 0.01
+        n = (n > threshold).astype(int)
+
+        return self.get_graph_from_matrix(n)
